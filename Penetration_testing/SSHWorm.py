d91d1b501063173a9b9d8b26416375f8aebd84e0
Sadauki <bello.bashirs@gmail.com>
2023-04-30 08:18:27 +0300

    Worm

---
+++ Penetration_testing/SSHWorm.py
@@ -0,0 +1,26 @@
+import pexpect
+
+PROMPT = ['# ', '>>> ', '> ', '\$']
+
+def send_command(child, cmd):
+    child.sendline(cmd)
+    child.expect(PROMPT)
+    print(child.before)
+
+def connect(user, host, password):
+    ssh_newkey = 'Are you sure you want to continue connecting'
+    connstr= 'ssh' + user +'@' + host
+    child = pexpect.spawn(connstr)
+    ret = child.expect([pexpect.TIMEOUT, ssh_newkey, '[P|p]assword:'])
+    if ret == 0:
+       print('[-] Error Connecting')
+       return
+    if ret == 1:
+        child.sendline('yes') 
+        ret = child.expect([pexpect.TIMEOUT, '[P|p]assword:'])
+        if ret == 0:
+           print('[-] Error connnecting')
+           return 
+    child.sendline(password)
+    child.expect(PROMPT)
+    return child
